3e33fcfd19cafd1b57feff20b8f20d4e4ccca2a1
mhgerov <mhgerov@gmail.com>
2017-12-27 14:06:30 -0500

    finished module11

---
+++ module10/javascript.js
@@ -0,0 +1,20 @@
+function grow() {
+    document.getElementById("box").style.height = "200px";
+    document.getElementById("box").style.width = "200px";
+
+}
+
+function blue() {
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+
+function fade() {
+    document.getElementById("box").style.opacity = "0.5";
+}
+
+function reset() {
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1";
+}
